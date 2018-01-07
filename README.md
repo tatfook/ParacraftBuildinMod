c1c056585614d4385c02c8aa0fafb53b2fb164ff
LiXizhi <lixizhi@yeah.net>
2018-01-08 00:47:12 +0800

    minor fix build script

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # ParacraftBuildinMod
 for making paracraft buildin mod package
 
-## How to Use
+## How to Install
+By default, this package is installed as a zip file in paracraft. 
 
 ### Method1: Use As a Package Zip File
 run 
@@ -22,3 +23,11 @@ Restart paracraft for the latest version of buildin-mod to take effect.
 - copy and merge all files to `Mod, script, textures` folder
 - zip folders as `./ParacraftBuildinMod.zip`
 
+## How to Code and Debug
+- 1: git clone this repository to paracraft's `npl_packages/ParacraftBuildinMod` folder
+- 2: run `build.bat` once
+- 3: edit repository code in `./npl_packages/*`
+- 4: run `build_without_update.bat` and test the code
+- repeat 3 and 4 until your code is ready
+- 5: commit and push your changes in `./npl_packages/*`
+- 6: run `build.bat` once
---
+++ build_without_update.bat
@@ -0,0 +1,55 @@
+@echo off 
+rem author: LiXizhi   date:2017.5.1
+
+rem update and install packages from git
+rem call InstallPackages.bat
+
+rem remove old redist folder
+rmdir Mod  /s /q
+rmdir textures  /s /q
+rmdir script  /s /q
+rmdir build  /s /q
+rmdir ParacraftBuildinMod  /s /q
+mkdir Mod
+mkdir textures
+mkdir script
+mkdir ParacraftBuildinMod
+
+pushd "npl_packages"
+
+CALL :BuddlePackage STLExporter
+CALL :BuddlePackage BMaxToParaXExporter
+
+CALL :BuddlePackage NPLCAD
+CALL :BuddlePackage NplCadLibrary
+CALL :BuddlePackage ModelVoxelizer
+
+CALL :BuddlePackage WorldShare
+CALL :BuddlePackage EMapMod
+
+
+popd
+
+rem copy files to ParacraftBuildinMod folder for packaging
+if exist Mod ( xcopy /s /y Mod  ParacraftBuildinMod\Mod\ )
+if exist textures ( xcopy /s /y textures  ParacraftBuildinMod\textures\ )
+if exist script ( xcopy /s /y script  ParacraftBuildinMod\script\ )
+xcopy /y package.npl  ParacraftBuildinMod\
+
+call "7z.exe" a ParacraftBuildinMod.zip ParacraftBuildinMod\
+xcopy /y/r ParacraftBuildinMod.zip  ..\
+rem start explorer.exe "%~dp0"
+
+
+EXIT /B %ERRORLEVEL%
+
+rem Mod, script, texture folder into root directory
+:BuddlePackage
+if exist "%1\Mod" (
+    xcopy /s /y %1\Mod  %1\..\..\Mod\
+	if exist %1\script ( xcopy /s /y %1\script  %1\..\..\script\ )
+	if exist %1\textures ( xcopy /s /y %1\textures  %1\..\..\textures\ )
+) else (
+    echo %1 is not a mod
+)
+EXIT /B 0
